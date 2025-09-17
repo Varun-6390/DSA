a904e4c3c998f8a3f26dc5815c6f3af9300bc6b1
Varun-6390 <artistvarunlko@gmail.com>
2025-09-17 21:44:40 +0530

    Find Number of even number of digits

---
+++ Even_Digit.java
@@ -0,0 +1,14 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+class Even_Digit{
+    public static void main(String[] args) {
+        int n = 809;
+        int count = 0;
+        while(n > 0)
+        {
+            count++;
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}
