0c71ddd536ae0dd34efda552ec64bc76b747c586
Varun-6390 <artistvarunlko@gmail.com>
2025-09-27 23:23:38 +0530

    Missing  Number

---
+++ MissingNumber.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        if (nums[0] != 0) {
+            return 0;
+        }
+        if (nums[n - 1] != n) {
+            return n;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+    //Optimised Solution
+    public int missingNumber2(int[] nums) {
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+        int k = 0;
+
+        for (int i : nums) {
+            k += i;
+        }
+        return sum - k;
+    }
+}
