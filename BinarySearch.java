c4ae453d422ca5f3a2cd91a2fb915853dcf43039
Varun-6390 <artistvarunlko@gmail.com>
2025-09-30 22:43:05 +0530

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+       int[] arr = {1,3,4,6,9,21,54,87,90};
+       int target = 21;
+       int ans = binarySearch(arr, target);
+       System.out.println("Element found at index: " + ans); 
+    }
+
+    static int binarySearch(int[] arr, int target)
+    {
+        int start = 0;
+        int end  = arr.length - 1;
+        
+
+        while(start <= end)
+        {
+            // int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
+
+            if(target > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else if(target < arr[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                return mid;
+            } 
+        }
+        return -1;
+    }
+}
