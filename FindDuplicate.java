0bdbb6c840b96255cc934b6a7fcb58109c97827a
Varun-6390 <artistvarunlko@gmail.com>
2025-10-08 22:23:28 +0530

    Find Duplicate

---
+++ FindDuplicate.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-the-duplicate-number
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+
+public class FindDuplicate {
+
+    static int findDuplicate(int[] nums) {
+        int i = 0;
+
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                int correct = nums[i] - 1;
+                if (nums[i] != nums[correct]) {
+                    swap(nums, i, correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
