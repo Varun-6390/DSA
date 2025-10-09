b1917dd02bd7a4d340fdf110b4de92ae10ee3a87
Varun-6390 <artistvarunlko@gmail.com>
2025-10-09 22:19:37 +0530

    Find All duplicates in array

---
+++ FindAllDuplicate.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array
+
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears at most twice, return an array of all the integers that appears twice.
+
+import java.util.List;
+import java.util.ArrayList;
+
+class FindAllSuplicate {
+    public List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                ans.add(nums[j]);
+            }
+        }
+        return ans;
+    }
+
+    void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
