3a520c4fedad4ee2bde0eb03b7f2dc5347ffcb05
Varun-6390 <artistvarunlko@gmail.com>
2025-10-09 22:20:25 +0530

    Find All Duplicates in array

--- FindAllDuplicate.java
+++ FindAllDuplicate.java
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array
-
 // Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears at most twice, return an array of all the integers that appears twice.
 
 import java.util.List;
@@ -31,3 +30,4 @@ class FindAllSuplicate {
         arr[b] = temp;
     }
 }
+
