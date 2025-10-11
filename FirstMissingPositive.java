8cf5535e4490807bcf09c0298ed83fc07e19cbc5
Varun-6390 <artistvarunlko@gmail.com>
2025-10-11 21:57:45 +0530

    First Missing Positive

---
+++ FirstMissingPositive.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/first-missing-positive
+
+//41. Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+public class FirstMissingPositive {
+    public int MissingPositive(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                return j + 1;
+            }
+        }
+        return arr.length + 1;
+
+    }
+
+    void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
