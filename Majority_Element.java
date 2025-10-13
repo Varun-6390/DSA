4018ac6d8c5d5b5c7deb8b8da8a7e0c8c85d79de
Varun-6390 <artistvarunlko@gmail.com>
2025-10-13 22:40:48 +0530

    Majority Elements

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,4 +1,6 @@
-// Given an array nums of size n, return the majority element.
+// https://leetcode.com/problems/majority-element
+
+//69. Given an array nums of size n, return the majority element.
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
 import java.util.Arrays;
@@ -16,3 +18,4 @@ public class Majority_Element {
         return nums[n/2];
     }
 }
+
