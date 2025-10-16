11b8f0333c84c7968518bbdd280d44693f5d1556
Varun-6390 <artistvarunlko@gmail.com>
2025-10-16 22:25:08 +0530

    Find first and last position of element in sorted array

--- FindFirstandLast.java
+++ FindFirstandLast.java
@@ -1,4 +1,6 @@
-// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+
+//34. Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 // If target is not found in the array, return [-1, -1].
 // You must write an algorithm with O(log n) runtime complexity.
 
@@ -54,4 +56,5 @@ public class FindFirstandLast {
         return ans;
     }
 }
+
 
