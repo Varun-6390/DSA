c77ae4a4ba8b59e9ffc3147935d57116b4476281
Varun-6390 <artistvarunlko@gmail.com>
2025-10-17 22:32:08 +0530

    Binary Search using Recursion

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -4,22 +4,18 @@ public class BinarySearchRecursion {
         int ans = search(nums, 12, 0, nums.length - 1);
         System.out.println("Element found at index: " + ans);
     }
-
     static int search(int[] arr, int target, int s, int e) {
         if (s > e) {
             return -1;
         }
         int mid = s + (e - s) / 2;
-
         if (arr[mid] == target) {
             return mid;
         }
-
         if (arr[mid] > target) {
             return search(arr, target, s, mid - 1);
         }
-
         return search(arr, target, mid + 1, e);
-
     }
 }
+
