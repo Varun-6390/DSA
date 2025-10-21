da381b17587c66e78dd91811c2766c37e480053d
Varun-6390 <artistvarunlko@gmail.com>
2025-10-21 22:09:41 +0530

    Linear Search

--- LinearSearch.java
+++ LinearSearch.java
@@ -5,7 +5,6 @@ public class LinearSearch {
         int ans = linearsearch(arr, 12);
         System.out.println("Element found at index: " + ans);
     }
-
     static int linearsearch(int[] nums, int target)
     {
         for(int i = 0; i < nums.length; i++)
