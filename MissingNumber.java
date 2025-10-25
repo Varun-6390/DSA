d3e3af1b2daf318de48c205eb80bce68ed9023e8
Varun-6390 <artistvarunlko@gmail.com>
2025-10-25 21:31:07 +0530

    Missing Number

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/missing-number
-
 //Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 import java.util.Arrays;
@@ -36,5 +35,6 @@ public class MissingNumber {
         return sum - k;
     }
 }
+
 
 
