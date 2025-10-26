816e0456cff00b2c97516faa744a155fede2ded5
Varun-6390 <artistvarunlko@gmail.com>
2025-10-26 22:34:07 +0530

    Update Single_Number.java

--- Single_Number.java
+++ Single_Number.java
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/single-number
-
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 
@@ -12,4 +11,5 @@ public class Single_Number {
         return index;
     }
 }
+
 
