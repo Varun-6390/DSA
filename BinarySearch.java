47ff68db5059049ea79ba54c66a0cf2ce8890e0c
Varun-6390 <artistvarunlko@gmail.com>
2025-10-31 22:22:35 +0530

    Binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,5 @@
+// Binsry Search
+
 public class BinarySearch {
     public static void main(String[] args) {
        int[] arr = {1,3,4,6,9,21,54,87,90};
@@ -31,4 +33,5 @@ public class BinarySearch {
         return -1;
     }
 }
+
 
