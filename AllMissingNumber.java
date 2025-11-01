b461839afa5f5f58305a90da98293d2c5f6e97e4
Varun-6390 <artistvarunlko@gmail.com>
2025-11-01 21:36:28 +0530

    Find all missing number in array

--- AllMissingNumber.java
+++ AllMissingNumber.java
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
-
 // Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] 
 // that do not appear in nums.
 
@@ -34,4 +33,5 @@ public class AllMissingNumber {
         arr[b] = temp;
     }
 }
+
 
