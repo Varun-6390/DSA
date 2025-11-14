d4bfb1ea89686ab1113aedb69b9a8a63f9e58b71
Varun-6390 <artistvarunlko@gmail.com>
2025-11-14 22:03:33 +0530

    Search Inserted position

--- Search_Inserted_position.java
+++ Search_Inserted_position.java
@@ -1,10 +1,8 @@
 // https://leetcode.com/problems/search-insert-position
-
 //35. Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
 //  return the index where it would be if it were inserted in order.
 
 public class Search_Inserted_position {
-
      public static int searchInsert(int[] nums, int target) {
        int low=0, high=nums.length-1;
         while(low<=high){
@@ -15,4 +13,3 @@ public class Search_Inserted_position {
         }return low; 
     }
 }
-
