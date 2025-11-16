770524dc53cc796a2342bc4a9b6a1b00de3d636d
Varun-6390 <artistvarunlko@gmail.com>
2025-11-16 22:22:09 +0530

    First Missing positive

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -21,12 +21,11 @@ public class FirstMissingPositive {
         return arr.length + 1;
 
     }
-
     void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-
 }
+
 
