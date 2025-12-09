e096362a0a64a2ac537396bcbbf296625a019056
Varun-6390 <artistvarunlko@gmail.com>
2025-12-09 22:17:31 +0530

    Find Number of steps to reduce a number to zero

---
+++ FindSteps.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
+// 1342. Number of Steps to Reduce a Number to Zero
+
+public class FindSteps {
+
+    static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+
+    static int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+        if (num % 2 == 0) {
+            return helper(num / 2, steps + 1);
+        }
+        return helper(num - 1, steps + 1);
+    }
+}
