bee14f012231c59fa3024abff36d39a54f7faab9
Varun-6390 <artistvarunlko@gmail.com>
2025-12-11 22:41:38 +0530

    Check the array is sorted is or not

---
+++ Sorted.java
@@ -0,0 +1,17 @@
+// Check whether the array is sorted or not
+
+public class Sorted {
+    public static void main(String[] args) {
+        int arr[] =  {2,4,5,7,9,10};
+        System.out.println(sorted(arr,0));
+    }
+
+    static boolean sorted(int[] arr, int index)
+    {
+        if(index == arr.length - 1)
+        {
+            return true;
+        }
+        return arr[index] < arr[index+1] && sorted(arr, index + 1);
+    }
+}
