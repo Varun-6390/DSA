6e968f0d19eeacbe26098fe244ffa9dacb939c3c
Varun-6390 <artistvarunlko@gmail.com>
2025-12-13 22:24:14 +0530

    Triangle pattern1 using recursion

---
+++ trianglePattern.java
@@ -0,0 +1,21 @@
+// Inverse Triangle Pattern using recursion
+
+public class trianglePattern {
+    public static void main(String[] args) {
+        triangle(6, 0);
+    }
+
+    static void triangle(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            System.out.print("*");
+            triangle(r, c + 1);
+        }
+        else{
+            System.out.println();
+            triangle(r-1, 0);
+        }
+    }
+}
