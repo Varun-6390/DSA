798f1ed8a67ee2df5cbd01b99c428610460fcf50
Varun-6390 <artistvarunlko@gmail.com>
2025-12-22 22:01:52 +0530

    All Combinations to make target using dice

---
+++ DiceComb.java
@@ -0,0 +1,24 @@
+// All Combinations to make target using a Dice 
+
+import java.util.ArrayList;
+
+public class DiceComb {
+    public static void main(String[] args) {
+        System.out.println(dice("", 4));
+    }
+
+    static ArrayList<String> dice(String p, int target) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+
+            list.addAll(dice(p + i, target - i));
+        }
+        return list;
+    }
+}
