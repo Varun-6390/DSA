cfbb1efa0e3f7e2706334059f3ebeaaaf4b372c6
Varun-6390 <artistvarunlko@gmail.com>
2025-12-23 21:45:18 +0530

    Grid path counting

---
+++ maze.java
@@ -0,0 +1,24 @@
+//Given a maze of matrix , you have to find all paths to reach from first cell to last. But you only move right and downwards
+
+public class maze {
+    public static void main(String[] args) {
+        path("", 4, 4);
+    }
+
+    static void path(String p, int r, int c)
+    {
+        if(r == 1 && c ==1)
+        {
+            System.out.println(p);
+        }
+
+        if(r > 1)
+        {
+            path(p + 'D', r - 1, c);
+        }
+        if(c > 1)
+        {
+            path(p + 'R', r, c - 1);
+        }
+    }
+}
