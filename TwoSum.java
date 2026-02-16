d87c7a6d03838d7aae198c60a2cf2dde97861cb7
Varun-6390 <artistvarunlko@gmail.com>
2026-02-16 21:48:20 +0530

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/two-sum/
+import java.util.Arrays;
+class TwoSum {
+    public static void main(String[] args) {
+        int arr[] = {2,7,11,15};
+        int target = 9;
+        int ans[] = twoSum(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        for(int i=0; i<nums.length-1; i++)
+        {
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
