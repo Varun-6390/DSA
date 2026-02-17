903ecb6ed7d668a1d861988309b813566672bfb6
Varun-6390 <artistvarunlko@gmail.com>
2026-02-17 22:25:03 +0530

    Valid Mountain Array

--- ValidMountainArray.java
+++ ValidMountainArray.java
@@ -6,28 +6,19 @@ class ValidMountainArray {
     }
     static boolean validMountainArray(int[] arr) {
         int n = arr.length;
-        
-        // Step 1: Length check
         if (n < 3) return false;
         
         int i = 0;
-        
-        // Step 2: Climb up
         while (i + 1 < n && arr[i] < arr[i + 1]) {
             i++;
         }
-        
-        // Step 3: Peak can't be first or last
         if (i == 0 || i == n - 1) {
             return false;
         }
-        
-        // Step 4: Walk down
         while (i + 1 < n && arr[i] > arr[i + 1]) {
             i++;
         }
-        
-        // Step 5: Must reach end
         return i == n - 1;
     }
 }
+
