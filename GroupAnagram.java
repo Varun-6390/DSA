47dd337a67a0a93bceca19d3c66316e1dfb9abfd
Varun-6390 <artistvarunlko@gmail.com>
2026-02-22 22:44:29 +0530

    Group Anagram

---
+++ GroupAnagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class GroupAnagram {
+    public static void main(String[] args) {
+        String[] str = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(str));
+    }
+        static List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String str : strs) {
+            
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            
+            map.get(sorted).add(str);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
