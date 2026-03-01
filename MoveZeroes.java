b528a87708262a2f83b15d73548342d2462b498c
Varun-6390 <artistvarunlko@gmail.com>
2026-03-01 22:18:51 +0530

    Move Zeroes

---
+++ MoveZeroes.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/move-zeroes?envType=problem-list-v2&envId=array
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }        
+    }
+}
