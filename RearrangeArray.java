e332a622067a3c0a2064a824e597fa5870fa48e0
Varun-6390 <artistvarunlko@gmail.com>
2026-03-06 22:06:50 +0530

    Rearrange Array

---
+++ RearrangeArray.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/rearrange-array-elements-by-sign
+import java.util.Arrays;
+
+public class RearrangeArray {
+    public static void main(String[] args) {
+        int[] arr = {3,1,-2,-5,2,-4};
+        int[] ans = rearrangeArray(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int a = 0;
+        int b = 1;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] > 0)
+            {
+                ans[a] = nums[i];
+                a += 2;
+            }
+            else
+            {
+                ans[b] = nums[i];
+                b += 2;
+            }
+        }
+        return ans;
+    }
+}
