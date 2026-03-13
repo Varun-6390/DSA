5f049451658080c17b58d4fb74d10dd36c1f7aa2
Varun-6390 <artistvarunlko@gmail.com>
2026-03-13 23:05:12 +0530

    Rotate Array

--- RotateArray.java
+++ RotateArray.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class RotateArray {
       public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
+        int arr[] = {1,2,3,4,5,6,7}; 
         int k = 3;
         rotate(arr,k);
         System.out.println(Arrays.toString(arr));
