3d6975f21bebfa07fd8313171ab4e748b0557827
Varun-6390 <artistvarunlko@gmail.com>
2026-03-22 22:01:26 +0530

    Remove Element

--- Remove_Element.java
+++ Remove_Element.java
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/remove-element
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
 //  The order of the elements may be changed. 
-//  Then return the number of elements in nums which are not equal to val. 
+//  Then return the number of elements in nums which are not equal to val.  
 
 
 class Remove_Element {
