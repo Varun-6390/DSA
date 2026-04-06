7740768519c3d7e135ca14df46a615566c26a036
Varun-6390 <artistvarunlko@gmail.com>
2026-04-06 22:11:46 +0530

    Smallest letter greater than target
    Restored comments explaining the function's purpose and behavior.

--- NextGreatestLetter.java
+++ NextGreatestLetter.java
@@ -2,7 +2,7 @@
 // You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
-public class NextGreatestLetter { 
+public class NextGreatestLetter {
     public char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
